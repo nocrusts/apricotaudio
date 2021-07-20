2644699078f075042cd88ab0d125705ab3317c2d
Thelolguy1 <18731514+Thelolguy1@users.noreply.github.com>
2021-07-20 14:35:29 -0700

    Adjusted positioning of more GUI buttons and fixed logic when switching songs.

--- __main__.py
+++ __main__.py
@@ -1,5 +1,6 @@
 import audioplayer
 import gi
+import math
 gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk
 
@@ -15,25 +16,38 @@ class Handler:
 
     def fileChosen(self, widget):  # when file chooser selects a file
         self.Audio = widget.get_filename()
-        print("Song path: " + str(self.Audio))
 
-    def playClicked(self, widget):
-        if self.PlayButtonMode == 0:
+        if self.sound == None:
             self.sound = audioplayer.PlaySound(self.Audio)
-            print("Song Length in ms: " + str(self.sound.audio_length))  # pydub uses ms
-            self.sound.play()
-            widget.set_label("gtk-media-pause")
-            self.PlayButtonMode = 1
         else:
             self.sound.stop()
-            print("Stopped Sound.")
+            self.sound = None  # not sure if this is needed or if python can handle this GC
+            self.sound = audioplayer.PlaySound(self.Audio)
             self.PlayButtonMode = 0
-            widget.set_label("gtk-media-play")
+            stop_start.set_label("gtk-media-play")
+
+        print("Song path: " + str(self.Audio))
+        print("Song Length in ms: " + str(self.sound.audio_length))  # pydub uses ms
+        STElapsed.set_text("0:00 / " + str(self.sound.DisplaySongLength))
+
+    def playClicked(self, widget):
+            if self.PlayButtonMode == 0:
+                self.sound.play()
+                widget.set_label("gtk-media-pause")
+                self.PlayButtonMode = 1
+            else:
+                self.sound.stop()
+                print("Stopped Sound.")
+                self.PlayButtonMode = 0
+                widget.set_label("gtk-media-play")
 
 
 builder = Gtk.Builder()
 builder.add_from_file("mainwindow.glade")
 builder.connect_signals(Handler())
 window = builder.get_object("window")
+stop_start = builder.get_object("song_play_pause")
+STElapsed = builder.get_object("song_time_elapsed")
+STRemaining = builder.get_object("song_time_remaining")
 window.show_all()
 Gtk.main()
--- audioplayer.py
+++ audioplayer.py
@@ -1,5 +1,7 @@
 # This file handles audio playback.
+# TODO: Fix MASSIVE corruption effect when playing audio(m4a only?) files.
 from pydub import AudioSegment
+import simpleaudio
 from pydub.playback import _play_with_simpleaudio
 
 
@@ -7,21 +9,47 @@ class PlaySound:
     def __init__(self, file):  # Used to initialize an audio object
         self.audio = AudioSegment.from_file(file)  # audiosegment object
         self.audio_length = len(self.audio)  # length of audio in ms
+        self._generateDisplaySongLength(self.audio_length)
         self.audio_pos = 0  # not in use
         self.isPlaying = None  # is it playing?
         self.playback = None  # simpleaudio object
+        self.DisplaySongLength  # the M:SS display
+        self.ms_elapsed = 0
 
     def play(self):  # Used to play an audio object
         if self.isPlaying:
             self.stop()  # stops existing sound thread.
         if AudioSegment:
-            self.playback = _play_with_simpleaudio(self.audio)  # https://github.com/jiaaro/pydub/issues/160
+            self.playback = simpleaudio.play_buffer(
+                self.audio.raw_data,
+                num_channels=self.audio.channels,
+                bytes_per_sample=self.audio.sample_width,
+                sample_rate=self.audio.frame_rate
+            ) # https://github.com/jiaaro/pydub/issues/160
         self.isPlaying = True
 
     def stop(self):  # Used to stop an audio object
         self.playback.stop()
         self.isPlaying = False
 
+    def _generateDisplaySongLength(self, ms):
+        #  1000ms is 1 second
+        #  60 seconds is 1 minute
+        #  60000 ms in 1 minute
+        seconds = int((ms/1000) % 60)
+        if seconds == 0:
+            seconds = "00"
+        minutes = int((ms/60000) % 60)
+        #  maybe adding hours at some point, not right now.
+        self.DisplaySongLength = str(minutes) + ":" + str(seconds)
+
+    #def _generateRemainingTime(self, ms_elapsed):
+    #
+    #
+    #def _generateElapsedTime(self, ms_elapsed):
+
+
 #  Workflow:
 #  Create a new var = PlaySound("file") object.
 #  Start audio with var.play()
+
