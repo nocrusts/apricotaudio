fa19a65823815b3df5925cd9ca5a65bb91cf4203
Thelolguy1 <18731514+Thelolguy1@users.noreply.github.com>
2021-07-19 23:03:59 -0700

    Play/Pause + Audio playback working!

--- __main__.py
+++ __main__.py
@@ -1,10 +1,39 @@
+import audioplayer
 import gi
 gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk
 
+
+class Handler:
+    def __init__(self):
+        self.PlayButtonMode = 0 # 0 is paused, 1 is playing
+        self.Audio = None  # tracks filename of audio file
+        self.sound = None  # audioplayer object
+
+    def onDestroy(self, *args):  # used for closing window
+        Gtk.main_quit()
+
+    def fileChosen(self, widget):  # when file chooser selects a file
+        self.Audio = widget.get_filename()
+        print("Song path: " + str(self.Audio))
+
+    def playClicked(self, widget):
+        if self.PlayButtonMode == 0:
+            self.sound = audioplayer.PlaySound(self.Audio)
+            print("Song Length in ms: " + str(self.sound.audio_length))  # pydub uses ms
+            self.sound.play()
+            widget.set_label("gtk-media-pause")
+            self.PlayButtonMode = 1
+        else:
+            self.sound.stop()
+            print("Stopped Sound.")
+            self.PlayButtonMode = 0
+            widget.set_label("gtk-media-play")
+
+
 builder = Gtk.Builder()
 builder.add_from_file("apricotmainwindow.glade")
+builder.connect_signals(Handler())
 window = builder.get_object("window")
 window.show_all()
-
 Gtk.main()
--- audioplayer.py
+++ audioplayer.py
@@ -1,15 +1,25 @@
 # This file handles audio playback.
-
 from pydub import AudioSegment
-from pydub.playback import play
+from pydub.playback import _play_with_simpleaudio
 
+class PlaySound:
+    def __init__(self, file):  # Used to initialize an audio object
+        self.audio = AudioSegment.from_file(file)  # audiosegment object
+        self.audio_length = len(self.audio)  # length of audio in ms
+        self.audio_pos = 0  # not in use
+        self.isPlaying = None  # is it playing?
+        self.playback = None  # simpleaudio object
 
-class Audio:
-    def __init__(self, file, filetype):
-        # self.name = filename # somehow get filename from file object, we'll figure this out when gtk is done.
-        self.type = filetype
-        self.sound = AudioSegment.from_file(file)
+    def play(self):  # Used to play an audio object
+        if self.isPlaying:
+            self.stop()  # stops existing sound thread.
+        if AudioSegment:
+            self.playback = _play_with_simpleaudio(self.audio)
+        self.isPlaying = True
 
+    def stop(self):  # Used to stop an audio object
+        self.playback.stop()
+        self.isPlaying = False
 
 
 
