834621eb6ff3c2b23f8566091bdc978f0941613a
Thelolguy1 <18731514+Thelolguy1@users.noreply.github.com>
2021-07-19 18:23:08 -0700

    GUI shows up!

--- __main__.py
+++ __main__.py
@@ -1,1 +1,10 @@
 import gi
+gi.require_version("Gtk", "3.0")
+from gi.repository import Gtk
+
+builder = Gtk.Builder()
+builder.add_from_file("apricotmainwindow.glade")
+window = builder.get_object("window")
+window.show_all()
+
+Gtk.main()
