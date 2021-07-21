b0022cb092bd766967bc2c616419554b4061d079
Thelolguy1 <18731514+Thelolguy1@users.noreply.github.com>
2021-07-20 17:46:11 -0700

    Slide-Selection working, I think...It does not move with progress though.

--- __main__.py
+++ __main__.py
@@ -1,15 +1,18 @@
 import audioplayer
 import gi
-import math
+import time
+import threading
+
 gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk
 
 
 class Handler:
     def __init__(self):
-        self.PlayButtonMode = 0 # 0 is paused, 1 is playing
+        self.PlayButtonMode = 0  # 0 is paused, 1 is playing
         self.Audio = None  # tracks filename of audio file
         self.sound = None  # audioplayer object
+        self.sliderPos = 0
 
     def onDestroy(self, *args):  # used for closing window
         Gtk.main_quit()
@@ -17,29 +20,44 @@ class Handler:
     def fileChosen(self, widget):  # when file chooser selects a file
         self.Audio = widget.get_filename()
 
-        if self.sound == None:
+        if self.sound is None:
             self.sound = audioplayer.PlaySound(self.Audio)
         else:
-            self.sound.stop()
-            self.sound = None  # not sure if this is needed or if python can handle this GC
+            try:
+                self.sound.stop()
+                self.sound = None  # not sure if this is needed or if python can handle this GC
+            except AttributeError:
+                print("Previous song was never played, catching exception.")
             self.sound = audioplayer.PlaySound(self.Audio)
             self.PlayButtonMode = 0
+            self.sliderPos = 0
             stop_start.set_label("gtk-media-play")
+            self.sound.ms_elapsed = 0
 
         print("Song path: " + str(self.Audio))
         print("Song Length in ms: " + str(self.sound.audio_length))  # pydub uses ms
-        STElapsed.set_text("0:00 / " + str(self.sound.DisplaySongLength))
+        STElapsed.set_text(" " + str(self.sound.DisplaySongLength))
 
     def playClicked(self, widget):
-            if self.PlayButtonMode == 0:
-                self.sound.play()
-                widget.set_label("gtk-media-pause")
-                self.PlayButtonMode = 1
-            else:
-                self.sound.stop()
-                print("Stopped Sound.")
-                self.PlayButtonMode = 0
-                widget.set_label("gtk-media-play")
+        if self.PlayButtonMode == 0:
+            print("Starting Sound.")
+            self.sound.play(0)
+            widget.set_label("gtk-media-pause")
+            self.PlayButtonMode = 1
+        else:
+            self.sound.stop()
+            print("Stopped Sound.")
+            self.PlayButtonMode = 0
+            widget.set_label("gtk-media-play")
+
+    def sliderMoved(self, gtkRange, scroll, value):
+        self.sliderPos = (max(min(int(value), 100), 0))
+
+    def sliderReleased(self, widget, event):
+        self.sound.play(int((self.sliderPos / 100) * self.sound.audio_length))
+
+    def sliderPressed(self, widget, event):
+        self.sound.stop()
 
 
 builder = Gtk.Builder()
--- audioplayer.py
+++ audioplayer.py
@@ -2,6 +2,7 @@
 # TODO: Fix MASSIVE corruption effect when playing audio(m4a only?) files.
 from pydub import AudioSegment
 import simpleaudio
+import io  # we don't want to make tempfiles for audiosegment to write to.
 from pydub.playback import _play_with_simpleaudio
 
 
@@ -10,22 +11,27 @@ class PlaySound:
         self.audio = AudioSegment.from_file(file)  # audiosegment object
         self.audio_length = len(self.audio)  # length of audio in ms
         self._generateDisplaySongLength(self.audio_length)
-        self.audio_pos = 0  # not in use
         self.isPlaying = None  # is it playing?
         self.playback = None  # simpleaudio object
         self.DisplaySongLength  # the M:SS display
-        self.ms_elapsed = 0
+        self._buffer = io.BytesIO()
 
-    def play(self):  # Used to play an audio object
+    def play(self, ms_position):  # Used to play an audio object
+        raw_data = self.audio.raw_data
         if self.isPlaying:
             self.stop()  # stops existing sound thread.
+        if ms_position:  # this insanity because simpleaudio REQUIRES channels, bytes per sample, and sample rate
+            processed_data = self.audio[ms_position:]
+            processed_data.export(self._buffer, format="raw")  # unsure if this is the best approach
+            raw_data = self._buffer.getvalue()
+
         if AudioSegment:
             self.playback = simpleaudio.play_buffer(
-                self.audio.raw_data,
+                raw_data,
                 num_channels=self.audio.channels,
                 bytes_per_sample=self.audio.sample_width,
                 sample_rate=self.audio.frame_rate
-            ) # https://github.com/jiaaro/pydub/issues/160
+            )  # https://github.com/jiaaro/pydub/issues/160
         self.isPlaying = True
 
     def stop(self):  # Used to stop an audio object
@@ -42,11 +48,6 @@ class PlaySound:
         minutes = int((ms/60000) % 60)
         #  maybe adding hours at some point, not right now.
         self.DisplaySongLength = str(minutes) + ":" + str(seconds)
-
-    #def _generateRemainingTime(self, ms_elapsed):
-    #
-    #
-    #def _generateElapsedTime(self, ms_elapsed):
 
 
 #  Workflow:
